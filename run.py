b2aa6c9a74fe7068005435a1198c593b02a9d3ee
Kevin Kepp <dev@kevinkepp.de>
2016-11-05 23:32:46 +0100

    Add random agent as baseline

--- run.py
+++ run.py
@@ -3,38 +3,44 @@ import random
 from lat.DeepQAgent import DeepQAgent
 from lat.Evaluator import Evaluator
 from lat.QAgent import QAgent
+from lat.RandomAgent import RandomAgent
 from lat.Simulator import Simulator, Actions
 from lat.KerasMlpModel import KerasMlpModel
 
-GRID_SIZE = 15
-MAX_STEPS = pow(GRID_SIZE, 1.7)
+
 EPOCHS = 2000
+GRID_SIZE = 6
+MAX_STEPS = pow(GRID_SIZE, 1.7)
+
+ACTIONS = Actions.all()
+agent = RandomAgent(ACTIONS)
+env_rand = Simulator(agent, MAX_STEPS, GRID_SIZE)
 
 ALPHA = 1  # optimal for deterministic env
 GAMMA = 0.975  #
 Q_INIT = lambda state, action: random.random()
-agent_q = QAgent(Actions.all(), ALPHA, GAMMA, Q_INIT)
+agent_q = QAgent(ACTIONS, ALPHA, GAMMA, Q_INIT)
 env_q = Simulator(agent_q, MAX_STEPS, GRID_SIZE)
 
 MODEL_IN_LAYER_SIZE = GRID_SIZE * GRID_SIZE
 MODEL_HID_LAYER_SIZES = [25]
-MODEL_OUT_LAYER_SIZE = len(Actions)
+MODEL_OUT_LAYER_SIZE = len(ACTIONS)
 model = KerasMlpModel(MODEL_IN_LAYER_SIZE, MODEL_HID_LAYER_SIZES, MODEL_OUT_LAYER_SIZE)
 EPSILON = 1
 EPSILON_UPDATE = lambda e: e - 1 / EPOCHS if e > 0.1 else e
-agent_deep_q = DeepQAgent(Actions.all(), GAMMA, EPSILON, EPSILON_UPDATE, model)
+agent_deep_q = DeepQAgent(ACTIONS, GAMMA, EPSILON, EPSILON_UPDATE, model)
 env_deep_q1 = Simulator(agent_deep_q, MAX_STEPS, GRID_SIZE)
 
 MODEL_HID_LAYER_SIZES = [150, 75]
 model = KerasMlpModel(MODEL_IN_LAYER_SIZE, MODEL_HID_LAYER_SIZES, MODEL_OUT_LAYER_SIZE)
-agent_deep_q = DeepQAgent(Actions.all(), GAMMA, EPSILON, EPSILON_UPDATE, model)
+agent_deep_q = DeepQAgent(ACTIONS, GAMMA, EPSILON, EPSILON_UPDATE, model)
 env_deep_q2 = Simulator(agent_deep_q, MAX_STEPS, GRID_SIZE)
 
 MODEL_HID_LAYER_SIZES = [300, 200, 100]
 model = KerasMlpModel(MODEL_IN_LAYER_SIZE, MODEL_HID_LAYER_SIZES, MODEL_OUT_LAYER_SIZE)
-agent_deep_q = DeepQAgent(Actions.all(), GAMMA, EPSILON, EPSILON_UPDATE, model)
+agent_deep_q = DeepQAgent(ACTIONS, GAMMA, EPSILON, EPSILON_UPDATE, model)
 env_deep_q3 = Simulator(agent_deep_q, MAX_STEPS, GRID_SIZE)
 
-envs = [env_q, env_deep_q1, env_deep_q2, env_deep_q3]
-names = ["QAgent", "DeepQAgent[25]", "DeepQAgent[150,100]", "DeepQAgent[300,200,100]"]
+envs = [env_rand, env_q, env_deep_q1, env_deep_q2, env_deep_q3]
+names = ["RandomAgent", "QAgent", "DeepQAgent[25]", "DeepQAgent[150,100]", "DeepQAgent[300,200,100]"]
 Evaluator(envs, names, EPOCHS, grid="{0}x{1}".format(GRID_SIZE, GRID_SIZE), gamma=GAMMA).run()
