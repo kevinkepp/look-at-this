52550a6bbb43ac8dc1b73dd841d7d76533e8d8f5
Kevin Kepp <dev@kevinkepp.de>
2016-11-05 15:42:56 +0100

    Fix bug in creating target for model update (used new state instead of old state for base), also use undiscounted reward for terminal states

--- lat/DeepQAgent.py
+++ lat/DeepQAgent.py
@@ -4,6 +4,9 @@ import numpy as np
 
 
 class DeepQAgent(RobotAgent):
+	_qs_old = None
+	_qs_old_state = None
+
 	# gamma: discount factor
 	# epsilon: epsilon-greedy strategy
 	# model: estimator for q values
@@ -16,6 +19,9 @@ class DeepQAgent(RobotAgent):
 
 	def choose_action(self, curr_state):
 		qs = self._model.predict_qs(curr_state)
+		# store qs for current state because usually we can use them in subsequent call to incorporate_reward
+		self._qs_old = qs
+		self._qs_old_state = curr_state
 		if random.random() < self._epsilon:
 			action = np.random.randint(0, self._actions_count)
 			print("Chosen action randomly")
@@ -25,11 +31,16 @@ class DeepQAgent(RobotAgent):
 		return action
 
 	def incorporate_reward(self, old_state, action, new_state, reward):
-		qs = self._model.predict_qs(new_state)
-		q_max = np.max(qs)
+		# retrieved stored qs for old state or re-predict them
+		qs_old = self._qs_old if np.array_equal(self._qs_old_state, old_state) else self._model.predict_qs(old_state)
 		target_qs = np.zeros((1, self._actions_count))
-		target_qs[:] = qs[:]
-		update = reward + (self._gamma * q_max)
+		target_qs[:] = qs_old[:]
+		qs_new = self._model.predict_qs(new_state)
+		q_max_new = np.max(qs_new)
+		if reward == -1:  # non-terminal
+			update = reward + (self._gamma * q_max_new)
+		else:  # terminal
+			update = reward
 		target_qs[0, action] = update
 		self._model.update_qs(old_state, target_qs)
 		print("Incorporated reward")
