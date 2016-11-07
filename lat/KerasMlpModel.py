427bd74cd684d6a4b25ea78b14c69e6ce87d0b31
Kevin Kepp <dev@kevinkepp.de>
2016-11-07 18:54:20 +0100

    Implement support for networks without hidden layer in  KerasMlpModel

--- lat/KerasMlpModel.py
+++ lat/KerasMlpModel.py
@@ -14,20 +14,26 @@ class KerasMlpModel(DeepQModel):
 
 	def _build_model(self):
 		model = Sequential()
+		has_hidden = self._l_hid_sizes is not None and self._l_hid_sizes != []
 
-		# add first hidden layer
-		model.add(Dense(self._l_hid_sizes[0], init='lecun_uniform', input_shape=(self._l_in_size,)))  # self._l_hid_size
-		model.add(Activation('relu'))
-		# model.add(Dropout(0.2))
-
-		# add other hidden layers (if given)
-		for hid_size in self._l_hid_sizes[1:]:
-			model.add(Dense(hid_size, init='lecun_uniform'))
+		if has_hidden:
+			# add first hidden layer
+			model.add(Dense(self._l_hid_sizes[0], init='lecun_uniform', input_shape=(self._l_in_size,)))
+			model.add(Dense(self._l_hid_sizes[0], init='lecun_uniform', input_shape=(self._l_in_size,)))
 			model.add(Activation('relu'))
-		# model.add(Dropout(0.2))
+			# model.add(Dropout(0.2))
+
+			# add other hidden layers (if given)
+			for hid_size in self._l_hid_sizes[1:]:
+				model.add(Dense(hid_size, init='lecun_uniform'))
+				model.add(Activation('relu'))
+			# model.add(Dropout(0.2))
 
 		# add output layer
-		model.add(Dense(self._l_out_size, init='lecun_uniform'))
+		if has_hidden:
+			model.add(Dense(self._l_out_size, init='lecun_uniform'))
+		else:
+			model.add(Dense(self._l_out_size, init='lecun_uniform', input_shape=(self._l_in_size,)))
 		model.add(Activation('linear'))  # linear output so we can have range of real-valued outputs
 
 		rms = RMSprop()
--- run.py
+++ run.py
@@ -8,8 +8,8 @@ from lat.Simulator import Simulator, Actions
 from lat.KerasMlpModel import KerasMlpModel
 
 
-EPOCHS = 2000
-GRID_SIZE = 6
+EPOCHS = 3000
+GRID_SIZE = 5
 MAX_STEPS = pow(GRID_SIZE, 1.7)
 
 ACTIONS = Actions.all()
@@ -23,7 +23,7 @@ agent_q = QAgent(ACTIONS, ALPHA, GAMMA, Q_INIT)
 env_q = Simulator(agent_q, MAX_STEPS, GRID_SIZE)
 
 MODEL_IN_LAYER_SIZE = GRID_SIZE * GRID_SIZE
-MODEL_HID_LAYER_SIZES = [25]
+MODEL_HID_LAYER_SIZES = []
 MODEL_OUT_LAYER_SIZE = len(ACTIONS)
 model = KerasMlpModel(MODEL_IN_LAYER_SIZE, MODEL_HID_LAYER_SIZES, MODEL_OUT_LAYER_SIZE)
 EPSILON = 1
@@ -31,16 +31,21 @@ EPSILON_UPDATE = lambda e: e - 1 / EPOCHS if e > 0.1 else e
 agent_deep_q = DeepQAgent(ACTIONS, GAMMA, EPSILON, EPSILON_UPDATE, model)
 env_deep_q1 = Simulator(agent_deep_q, MAX_STEPS, GRID_SIZE)
 
-MODEL_HID_LAYER_SIZES = [150, 75]
+MODEL_HID_LAYER_SIZES = []
 model = KerasMlpModel(MODEL_IN_LAYER_SIZE, MODEL_HID_LAYER_SIZES, MODEL_OUT_LAYER_SIZE)
 agent_deep_q = DeepQAgent(ACTIONS, GAMMA, EPSILON, EPSILON_UPDATE, model)
 env_deep_q2 = Simulator(agent_deep_q, MAX_STEPS, GRID_SIZE)
 
-MODEL_HID_LAYER_SIZES = [300, 200, 100]
+MODEL_HID_LAYER_SIZES = [50]
+model = KerasMlpModel(MODEL_IN_LAYER_SIZE, MODEL_HID_LAYER_SIZES, MODEL_OUT_LAYER_SIZE)
+agent_deep_q = DeepQAgent(ACTIONS, GAMMA, EPSILON, EPSILON_UPDATE, model)
+env_deep_q2 = Simulator(agent_deep_q, MAX_STEPS, GRID_SIZE)
+
+MODEL_HID_LAYER_SIZES = [150, 75]
 model = KerasMlpModel(MODEL_IN_LAYER_SIZE, MODEL_HID_LAYER_SIZES, MODEL_OUT_LAYER_SIZE)
 agent_deep_q = DeepQAgent(ACTIONS, GAMMA, EPSILON, EPSILON_UPDATE, model)
 env_deep_q3 = Simulator(agent_deep_q, MAX_STEPS, GRID_SIZE)
 
 envs = [env_rand, env_q, env_deep_q1, env_deep_q2, env_deep_q3]
-names = ["RandomAgent", "QAgent", "DeepQAgent[25]", "DeepQAgent[150,100]", "DeepQAgent[300,200,100]"]
+names = ["RandomAgent", "QAgent", "DeepQAgent[]", "DeepQAgent[50]", "DeepQAgent[150,100]"]
 Evaluator(envs, names, EPOCHS, grid="{0}x{1}".format(GRID_SIZE, GRID_SIZE), gamma=GAMMA).run()
