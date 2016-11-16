9ffe27b16e49e907cfeb05a8c9888d7aa00c177b
philibb <phil89b@gmail.com>
2016-11-16 14:46:06 +0100

    path visualization works (added my testarea.py to gitignore)

--- lat/SimpleVisualize.py
+++ lat/SimpleVisualize.py
@@ -13,35 +13,48 @@ class PlotMatrix(Visualizer):
 		""" just plots current state in a simple print of matrix"""
 		print(state)
 
-	def visualize_course_of_action(self, first_state, actions, image_name="agent_path.png"):
+	def visualize_course_of_action(self, world_state, first_i, first_j, grid_n, grid_m, actions, title=None, image_name="agent_path.png"):
 		""" plots a course of actions beginning from a certain first state """
-		n,m = first_state.shape
-		y,x = np.where(first_state==1)
-		x = x[0]
-		y = y[0]
-		X = np.array(x)
-		Y = np.array(y)
+		n, m = grid_n, grid_m
+		x = first_j + m//2
+		y = first_i + n//2
+		xx = np.array(x)
+		yy = np.array(y)
 		for ac in actions:
 			(x,y) = self._get_new_xy(x,y,ac)
-			X = np.append(X,x)
-			Y = np.append(Y,y)
+			xx = np.append(xx,x)
+			yy = np.append(yy,y)
 		plt.figure
-		plt.plot(X,-Y,'b-',X[0],-Y[0],'ro',X[-1],-Y[-1],'rx')
-		plt.xlim((-0.5,m-0.5))
-		plt.ylim((-n+0.5,0.5))
+		plt.plot(xx, yy, 'b-', xx[-1], yy[-1], 'ro')
+		# plotting starting view window
+		first_win_x = np.array([first_j, first_j, first_j + m, first_j + m, first_j])
+		first_win_y = np.array([first_i, first_i+n, first_i+n, first_i, first_i])
+		plt.plot(first_win_x, first_win_y, 'r:')
+		# plotting final view window
+		mid_m = m//2
+		mid_n = n//2
+		final_win_x = np.array([xx[-1]-mid_m, xx[-1]-mid_m, xx[-1]+mid_m, xx[-1]+mid_m, xx[-1]-mid_m])
+		final_win_y = np.array([yy[-1]-mid_n, yy[-1]+mid_n, yy[-1]+mid_n, yy[-1]-mid_n, yy[-1]-mid_n])
+		plt.plot(final_win_x, final_win_y, 'r:')
+		# plot world-frame
+		plt.imshow(world_state, cmap="gray", alpha=0.8, interpolation='none')
+		# remove x & y axis ticks
 		plt.xticks([])
 		plt.yticks([])
+		if title is not None:
+			plt.title(title)
+		# save and clear figure
 		plt.savefig(image_name)
 		plt.clf()
 
-	def _get_new_xy(self, x, y, ac):
+	def _get_new_xy(x, y, ac):
 		""" calculates the new position of the goal after a action """
 		if ac == Actions.up:
-			y += 1
-		elif ac == Actions.down:
 			y -= 1
+		elif ac == Actions.down:
+			y += 1
 		elif ac == Actions.right:
-			x -= 1
-		elif ac == Actions.left:
 			x += 1
-		return (x,y)
+		elif ac == Actions.left:
+			x -= 1
+		return x, y
