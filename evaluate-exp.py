69ab6cc927d80975741c9d13b24aa4fc40b8d8b7
philibb <phil89b@gmail.com>
2016-12-31 11:30:39 +0100

    corrected evaluate-exp.py for current proper use

--- evaluate-exp.py
+++ evaluate-exp.py
@@ -1,26 +1,26 @@
 from sft.eval.Evaluator import Evaluator
+import os
 
-EXPERIMENT_PATH = "/home/philibb/Dropbox/Uni/Robotics Project/github-repo/tmp/logs/20161220-135155_exp1"
+LOG_PATH = "/home/philibb/Dropbox/Uni/Robotics Project/github-repo/tmp/logs"
+EXPERIMENT = "20161231-094648_exp1"
 WORLD_DIR = "world"
 AGENTS_DICT = {
-	"Deep[1000,200]": "agent0",
-	"Deep[16,8]": "agent1"}
+	"Deep[16]": "agent0"}
 
 # results
 SLIDING_MEAN_WINDOW = 100
 
 # paths
-PLOT_EVERY_KTH_EPOCH = 100
-NUM_PLOT_PATHS_IN_ROW = 5
+PLOT_EVERY_KTH_EPOCH = 3
+NUM_PLOT_PATHS_IN_ROW = 1
 
-# parameters
-
+EXPERIMENT_PATH = os.path.join(LOG_PATH, EXPERIMENT)
+# create evaluator
 evaluator = Evaluator(EXPERIMENT_PATH, WORLD_DIR, AGENTS_DICT)
-
+# plot results, paths, qs
 evaluator.plot_paths(PLOT_EVERY_KTH_EPOCH, NUM_PLOT_PATHS_IN_ROW)
-
 evaluator.plot_results(AGENTS_DICT.keys(), SLIDING_MEAN_WINDOW)
+evaluator.plot_qs(AGENTS_DICT.keys(), "q.tsv")
 
 #evaluator.plot_epsilon()
 #evaluator.plot_expansive_spl_radius()
-evaluator.plot_qs()
