b23601e4f6b813084c8372c0abe1e52f76fe00ba
Kevin Kepp <dev@kevinkepp.de>
2017-01-12 01:46:38 +0100

    Revert change from previous commit

--- sft/agent/model/KerasMlpModel.py
+++ sft/agent/model/KerasMlpModel.py
@@ -24,31 +24,16 @@ class KerasMlpModel(DeepQModel):
 		self._model = model
 
 	def predict_qs(self, state):
-		# assume one channel and one sample
-		X = [np.array([[l]]) for l in state.to_list()]
-		# X shapes: [ (1, 1, view_size.w, view_size.h), (1, 1, action_history_len, nb_actions) ]
-		p = self._model.predict(X, batch_size=1, verbose=0)
+		x = state.flatten()
+		assert x.shape[1] == self.layers[0].input_shape[1]
+		p = self._model.predict(x, batch_size=1, verbose=0)
 		return p
 
 	def update_qs(self, states, targets):
-		n_samples = len(states)
-		assert len(states) > 0
-		list_length = len(states[0].to_list())
-		X = []
-		for i in range(list_length):
-			X.append([])
-		for state in states:
-			for i, v in enumerate(state.to_list()):
-				v = [v]  # assume one channel
-				X[i].append(v)
-		for i, v in enumerate(X):
-			X[i] = np.array(v)
-		# X shapes: [ (n_samples, 1, view_size.w, view_size.h), (n_samples, 1, action_history_len, nb_actions) ]
+		X = State.flatten_states(states)
+		assert X.shape[1] == self.layers[0].input_shape[1]
+		n_samples = X.shape[0]
 		self._model.fit(X, targets, batch_size=n_samples, nb_epoch=1, verbose=0)
-		# weights = self._model.get_weights()
-		# means = [np.mean(np.abs(w)) for w in weights]
-		# maxs = [np.max(np.abs(w)) for w in weights]
-		# print "weights - maxs: %s, means: %s" % (str(maxs), str(means))
 
 	def load(self, file_path):
 		self.logger.log_message("Load model from {0}".format(file_path))
