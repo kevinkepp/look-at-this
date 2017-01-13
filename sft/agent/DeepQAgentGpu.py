f4140ebd431f47819fb0bc04b8110f893a817e0f
Kevin Kepp <dev@kevinkepp.de>
2017-01-13 02:19:27 +0100

    Implement action history

--- sft/agent/DeepQAgentGpu.py
+++ sft/agent/DeepQAgentGpu.py
@@ -7,21 +7,25 @@ from sft.agent.RobotAgent import RobotAgent
 
 
 class ReplayBuffer(object):
-	def __init__(self, size, view_size):
+	def __init__(self, size, view_size, action_hist_size):
 		self.size = size
 		self.len = 0
 		self.top = 0
-		self.s = np.zeros((size, 1, view_size.w, view_size.h), dtype=theano.config.floatX)
+		self.v = np.zeros((size, 1, view_size.w, view_size.h), dtype=theano.config.floatX)
+		self.ah = np.zeros((size, 1, action_hist_size.w, action_hist_size.h), dtype=theano.config.floatX)
 		self.a = np.zeros((size, 1), dtype=np.int32)
-		self.s2 = np.zeros((size, 1, view_size.w, view_size.h), dtype=theano.config.floatX)
+		self.v2 = np.zeros((size, 1, view_size.w, view_size.h), dtype=theano.config.floatX)
+		self.ah2 = np.zeros((size, 1, action_hist_size.w, action_hist_size.h), dtype=theano.config.floatX)
 		self.r = np.zeros((size, 1), dtype=theano.config.floatX)
 		self.t = np.zeros((size, 1), dtype=np.int32)
 
-	def add(self, s, a, s2, r, t):
+	def add(self, v, ah, a, v2, ah2, r, t):
 		# write to current index
-		self.s[self.top] = s
+		self.v[self.top] = v
+		self.ah[self.top] = ah
 		self.a[self.top] = a
-		self.s2[self.top] = s2
+		self.v2[self.top] = v2
+		self.ah2[self.top] = ah2
 		self.r[self.top] = r
 		self.t[self.top] = t
 		self.top += 1
@@ -32,7 +36,8 @@ class ReplayBuffer(object):
 
 	def draw_batch(self, batch_size):
 		indices = np.random.randint(0, self.size, batch_size)
-		return self.s[indices], self.a[indices], self.s2[indices], self.r[indices], self.t[indices]
+		return self.v[indices], self.ah[indices], self.a[indices], self.v2[indices], self.ah2[indices], \
+			   self.r[indices], self.t[indices]
 
 
 class DeepQAgentGpu(RobotAgent):
@@ -43,12 +48,12 @@ class DeepQAgentGpu(RobotAgent):
 	# buffer size: size of experience pool from which minibatches are randomly sampled
 	# start_learn: after how many experiences (buffer size) we start learning based on experiences
 	# learn_steps: every learn_steps steps the model gets updated
-	def __init__(self, logger, actions, batch_size, buffer_size, start_learn, learn_steps, view_size, model):
+	def __init__(self, logger, actions, batch_size, buffer_size, start_learn, learn_steps, view_size, action_hist_size, model):
 		self.logger = logger
 		self.actions = actions
 		self.batch_size = batch_size
 		buffer_size = max(buffer_size, batch_size)  # buffer has to be at least batch_size
-		self.replay_buffer = ReplayBuffer(buffer_size, view_size)
+		self.replay_buffer = ReplayBuffer(buffer_size, view_size, action_hist_size)
 		self.start_learn = start_learn
 		self.learn_steps = learn_steps
 		self.steps = 0
@@ -56,7 +61,7 @@ class DeepQAgentGpu(RobotAgent):
 		self.model = model
 
 	def choose_action(self, curr_state, eps):
-		qs = self.model.predict_qs(curr_state.view)
+		qs = self.model.predict_qs(curr_state.view, curr_state.actions)
 		self.logger.log_parameter("q", qs)
 		# store qs for current state because usually we can use them in subsequent call to incorporate_reward
 		if random.random() < eps:
@@ -68,11 +73,13 @@ class DeepQAgentGpu(RobotAgent):
 	def incorporate_reward(self, old_state, action, new_state, reward):
 		""" incorporates reward, states, action into replay list and updates the parameters of model """
 		self.logger.log_parameter("reward", reward)
-		old_state = old_state.view
+		old_view = old_state.view
+		old_action_hist = old_state.actions
 		is_terminal = new_state is None
 		terminal = 1 if is_terminal else 0
-		new_state = new_state.view if not is_terminal else np.zeros(old_state.shape)
-		exp_new = (old_state, action, new_state, reward, terminal)
+		new_view = new_state.view if not is_terminal else np.zeros(old_view.shape)
+		new_action_hist = new_state.actions if not is_terminal else np.zeros(old_action_hist.shape)
+		exp_new = (old_view, old_action_hist, action, new_view, new_action_hist, reward, terminal)
 		self.replay_buffer.add(*exp_new)
 		if self.replay_buffer.len >= self.start_learn and self.steps % self.learn_steps == 0:
 			minibatch = self.replay_buffer.draw_batch(self.batch_size)
--- sft/agent/model/LasagneMlpModel.py
+++ sft/agent/model/LasagneMlpModel.py
@@ -6,68 +6,86 @@ import lasagne.layers
 
 
 class SharedBatch(object):
-	def __init__(self, size, view_size):
+	def __init__(self, size, view_size, action_history_size):
 		self.size = size
-		self.s = shared(np.zeros((self.size, 1, view_size.w, view_size.h), dtype=theano.config.floatX))
+		self.v = shared(np.zeros((self.size, 1, view_size.w, view_size.h), dtype=theano.config.floatX))
+		self.ah = shared(np.zeros((self.size, 1, action_history_size.w, action_history_size.h), dtype=theano.config.floatX))
 		self.a = shared(np.zeros((self.size, 1), dtype=np.int32), broadcastable=(False, True))
-		self.s2 = shared(np.zeros((self.size, 1, view_size.w, view_size.h), dtype=theano.config.floatX))
+		self.v2 = shared(np.zeros((self.size, 1, view_size.w, view_size.h), dtype=theano.config.floatX))
+		self.ah2 = shared(np.zeros((self.size, 1, action_history_size.w, action_history_size.h), dtype=theano.config.floatX))
 		self.r = shared(np.zeros((self.size, 1), dtype=theano.config.floatX), broadcastable=(False, True))
 		self.t = shared(np.zeros((self.size, 1), dtype=np.int32), broadcastable=(False, True))
 
-	def set(self, s, a, s2, r, t):
-		self.s.set_value(s)
+	def set(self, v, ah, a, v2, ah2, r, t):
+		self.v.set_value(v)
+		self.ah.set_value(ah)
 		self.a.set_value(a)
-		self.s2.set_value(s2)
+		self.v2.set_value(v2)
+		self.ah2.set_value(ah2)
 		self.r.set_value(r)
 		self.t.set_value(t)
 
-	def givens(self, states, actions, next_states, rewards, terminals):
+	def givens(self, views, action_hists, actions, next_states, next_action_hists, rewards, terminals):
 		return {
-			states: self.s,
+			views: self.v,
+			action_hists: self.ah,
 			actions: self.a,
-			next_states: self.s2,
+			next_states: self.v2,
+			next_action_hists: self.ah2,
 			rewards: self.r,
 			terminals: self.t
 		}
 
 
 class SharedState(object):
-	def __init__(self, view_size):
+	def __init__(self, view_size, action_hist_size):
 		self.view_size = view_size
-		self.s = theano.shared(np.zeros((1, view_size.w, self.view_size.h), dtype=theano.config.floatX))
+		self.action_hist_size = action_hist_size
+		self.v = theano.shared(np.zeros((view_size.w, view_size.h), dtype=theano.config.floatX))
+		self.ah = theano.shared(np.zeros((action_hist_size.w, action_hist_size.h), dtype=theano.config.floatX))
 
-	def set(self, s):
-		self.s.set_value(s)
+	def set(self, v, ah):
+		self.v.set_value(v)
+		self.ah.set_value(ah)
 
-	def givens(self, states):
-		return {states: self.s.reshape((1, 1, self.view_size.w, self.view_size.h))}
+	def givens(self, views, action_hists):
+		return {
+			views: self.v.reshape((1, 1, self.view_size.w, self.view_size.h)),
+			action_hists: self.ah.reshape((1, 1, self.action_hist_size.w, self.action_hist_size.h))
+		}
 
 
 class LasagneMlpModel(object):
-	def __init__(self, logger, batch_size, discount, actions, learning_rate, view_size, network, optimizer):
+	def __init__(self, logger, batch_size, discount, actions, learning_rate, view_size, action_hist_size,
+				 network_input_view, network_input_actions, network_output, optimizer):
 		self.logger = logger
 		self.discount = discount
 		self.actions = actions
 		self.learning_rate = learning_rate
 		self.view_size = view_size
-		self.network = network
+		self.action_hist_size = action_hist_size
+		self.net_in_view = network_input_view
+		self.net_in_actions = network_input_actions
+		self.net_out = network_output
 		self.optimizer = optimizer
-		self.shared_batch = SharedBatch(batch_size, view_size)
-		self.shared_state = SharedState(view_size)
+		self.shared_batch = SharedBatch(batch_size, view_size, action_hist_size)
+		self.shared_state = SharedState(view_size, action_hist_size)
 		self.train_fn = None
 		self.predict_fn = None
 		self.build_model()
 
 	def build_model(self):
-		states = T.tensor4('states')
+		views = T.tensor4('views')
+		action_hists = T.tensor4('action_hists')
 		actions = T.icol('actions')
-		next_states = T.tensor4('next_states')
+		next_views = T.tensor4('next_views')
+		next_action_hists = T.tensor4('next_action_hists')
 		rewards = T.col('rewards')
 		terminals = T.icol('terminals')
 
 		# build loss function based on q value predictions
-		q_vals = lasagne.layers.get_output(self.network, states)
-		next_q_vals = lasagne.layers.get_output(self.network, next_states)
+		q_vals = lasagne.layers.get_output(self.net_out, {self.net_in_view: views, self.net_in_actions: action_hists})
+		next_q_vals = lasagne.layers.get_output(self.net_out, {self.net_in_view: next_views, self.net_in_actions: next_action_hists})
 		actionmask = T.eq(T.arange(4).reshape((1, -1)), actions.reshape((-1, 1))).astype(theano.config.floatX)
 		terminals_float = terminals.astype(theano.config.floatX)
 		target = rewards + \
@@ -79,21 +97,21 @@ class LasagneMlpModel(object):
 		loss = T.sum(loss)  # batch accumulator == 'sum'
 
 		# build train function
-		params = lasagne.layers.helper.get_all_params(self.network)
+		params = lasagne.layers.helper.get_all_params(self.net_out)
 		updates = self.optimizer(loss, params)
-		train_givens = self.shared_batch.givens(states, actions, next_states, rewards, terminals)
+		train_givens = self.shared_batch.givens(views, action_hists, actions, next_views, next_action_hists, rewards, terminals)
 		self.train_fn = theano.function([], [loss], updates=updates, givens=train_givens)
 
 		# build predict function
-		predict_givens = self.shared_state.givens(states)
+		predict_givens = self.shared_state.givens(views, action_hists)
 		self.predict_fn = theano.function([], q_vals[0], givens=predict_givens)
 
-	def predict_qs(self, state):
-		self.shared_state.set(state.reshape(1, self.view_size.w, self.view_size.h))
+	def predict_qs(self, views, action_hists):
+		self.shared_state.set(views, action_hists)
 		return self.predict_fn()
 
-	def update_qs(self, states, actions, next_states, rewards, terminals):
-		self.shared_batch.set(states, actions, next_states, rewards, terminals)
+	def update_qs(self, views, action_hists, actions, next_views, next_action_hists, rewards, terminals):
+		self.shared_batch.set(views, action_hists, actions, next_views, next_action_hists, rewards, terminals)
 		loss = self.train_fn()
 		return np.sqrt(loss)
 
