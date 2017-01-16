Fix floating point precision in action history (contained in state)

sft/Trainer.py
@@ -7,6 +7,7 @@ import threading
 import time
 import random
 import sys
+import theano
 
 from sft.Actions import Actions
 from sft.State import State
@@ -111,7 +112,7 @@ class Trainer(object):
 			return -1
 
 	def get_state(self, view, action_hist, state_action_hist_len):
-		actions = np.zeros([state_action_hist_len, len(Actions.all)])
+		actions = np.zeros([state_action_hist_len, len(Actions.all)], dtype=theano.config.floatX)
 		# take last n actions, this will be smaller or empty if there are not enough actions
 		last_actions = action_hist[-state_action_hist_len:] if state_action_hist_len > 0 else []
 		for i in range(len(last_actions)):

sft/agent/DeepQAgentGpu.py
@@ -77,8 +77,8 @@ class DeepQAgentGpu(RobotAgent):
 		old_action_hist = old_state.actions
 		is_terminal = new_state is None
 		terminal = 1 if is_terminal else 0
-		new_view = new_state.view if not is_terminal else np.zeros(old_view.shape)
-		new_action_hist = new_state.actions if not is_terminal else np.zeros(old_action_hist.shape)
+		new_view = new_state.view if not is_terminal else np.zeros(old_view.shape, dtype=theano.config.floatX)
+		new_action_hist = new_state.actions if not is_terminal else np.zeros(old_action_hist.shape, dtype=theano.config.floatX)
 		exp_new = (old_view, old_action_hist, action, new_view, new_action_hist, reward, terminal)
 		self.replay_buffer.add(*exp_new)
 		if self.replay_buffer.len >= self.start_learn and self.learn_steps % self.learn_at_steps == 0:

sft/agent/model/LasagneMlpModel.py
@@ -146,7 +146,7 @@ class LasagneMlpModel(object):
 		if self.steps_clone == self.clone_interval - 1:
 			param_values_next = lasagne.layers.get_all_param_values(self.net_out_next)
 			param_values_org = lasagne.layers.get_all_param_values(self.net_out)
-			diffs = np.zeros(len(param_values_org))
+			diffs = np.zeros(len(param_values_org), dtype=theano.config.floatX)
 			for i in range(len(param_values_org)):
 				diffs[i] = np.sum(param_values_next[i] - param_values_org[i])
 			diff = np.sum(diffs)