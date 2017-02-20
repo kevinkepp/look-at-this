94171a65431ab6216f42f3672c620e204e2dc046
Philibb <phil89b@gmail.com>
2017-02-20 19:35:05 +0100

    delete obsolete and unnecessary  run_eval.py script

--- run_eval.py
+++
@@ -1,5 +0,0 @@
-from sft.eval.visual.PathVisualizer import PathVisualizer
-
-# visualize paths based on output directory
-print("RuntimeWarnings related to imports can be ignored")
-PathVisualizer().visualize_paths("tmp/logs/20161229-174200_exp", "sft.config.exp", "no-cloning")
